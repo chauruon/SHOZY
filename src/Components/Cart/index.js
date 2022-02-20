fc417465b8ab72420285dedba3e96a3031a92840
chauruon <chauruonangiang@gmail.com>
2022-02-20 19:38:02 +0700

    cart sreen by branch ruon

---
+++ src/Components/Cart/index.js
@@ -0,0 +1,4 @@
+import React, {Component, useState } from 'react';
+import {TextInput,Switch, StyleSheet, TouchableOpacity,SafeAreaView ,Text, Dimensions, Appearance, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import style from './styles';
