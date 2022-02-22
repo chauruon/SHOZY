3df6a38d6e0e5aac10f42051c830be54c1d6c1f8
chauruon <chauruonangiang@gmail.com>
2022-02-22 16:55:02 +0700

    view cart

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /**
  * @format
  */
-import { extendTheme, NativeBaseProvider } from 'native-base';
+// import { extendTheme, NativeBaseProvider } from 'native-base';
 import {AppRegistry} from 'react-native';
 import React from 'react';
-import App from './src/Navigator/Main';
+// import App from './src/Navigator/Main';
+import App from './src/Components/Cart';
 import {name as appName} from './app.json';
-import configureStore from './store';
-import { Provider } from 'react-redux';
-const store = configureStore();
+// import configureStore from './store';
+// import { Provider } from 'react-redux';
+// const store = configureStore();
 
 const RNRedux = () => {
     return (
@@ -20,4 +21,4 @@ const RNRedux = () => {
     )
 }
 
-AppRegistry.registerComponent(appName, () => RNRedux);
+AppRegistry.registerComponent(appName, () => App);
